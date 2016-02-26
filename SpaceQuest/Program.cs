cb16a7135ea4a4580e388cd48d3fe3e1f86cd502
andrewgalb <andrew.f.galbraith@edu.varnamo.se>
2016-02-26 08:16:49 +0100

    Hackathon 1

---
+++ SpaceQuest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("\tWELCOME TO SPACE QUEST");
+
+        Cockpit cockpit = new Cockpit("Cockpit", 4);
+
+    }
+}
+
---
+++ SpaceQuest/Room.cs
@@ -0,0 +1,72 @@
+﻿using System;
+class Room
+{
+    private string name;
+    //Hazard multiplier
+    protected int hzM;
+    protected int life = 100;
+
+    public Room(string name, int hzM)
+    {
+        this.name = name;
+        this.hzM = hzM;
+    }
+    virtual public int die()
+    {
+        return life;
+    }
+}
+
+class Cockpit : Room
+{
+    public Cockpit(string name, int hzM) : base(name, hzM)
+    {
+    }
+
+    public int brake(string choice)
+    {
+        if(choice == "pull brake")
+        {
+            Console.WriteLine("Success");
+            return life;
+        }
+        else
+        {
+            return life -= 10 * hzM;
+        }
+    }
+}
+
+class EngineRoom : Room
+{
+    public EngineRoom(string name, int hzM) : base(name, hzM)
+    {
+    }
+
+    public int fire(string choice)
+    {
+        if(choice == "spit")
+        {
+            return life -= 10 * hzM;
+        }
+        else if(choice == "use fire extinguisher")
+        {
+            return life -= 10 * hzM;
+        }
+        else if(choice == "use force lightning")
+        {
+            return life;
+        }
+        else
+        {
+            Console.WriteLine("You had one job. Choose one of the three. Time wasted..");
+            return life -= 1;
+        }
+
+    }
+}
+
+//class EscapePod : Room
+
+    //SKa alltid kunna rymma till escapepod - Game over - Chicken out - Putin överlever - Strippklubb förstörs
+
